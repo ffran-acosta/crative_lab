0143e7330b68aa9f752f20bc4abde77e37b24078
Franco Acosta <francoaugustoacosta@gmail.com>
2024-02-19 22:09:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto bg-slate-400">
+      <h1 className="text-3xl font-bold underline">Título de mi aplicación</h1>
+    </div>
+  );
+}
+
+export default App;
