ff746d380e0093e12aea0dfd85f19fce1e2a164b
Franco Acosta <francoaugustoacosta@gmail.com>
2024-02-27 20:40:46 -0300

    content creation test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import Logo from '../assets/logo/logo.png'
+import Logo from '../assets/logo/logo.png';
+
 const Header = () => {
     return (
         <header className="bg-transparent">
             <nav className="flex justify-between items-center">
-                <a href="/" className="text-[#bd6097] hover:text-[#834167] text-5xl ml-5">MENU</a>
-                <img src={Logo} alt="Creative Lab Logo" className="max-w-[250px] mt-5" />
-                <a href="/about-us" className="text-[#bd6097] hover:text-[#834167] text-5xl mr-5">ABOUT US</a>
+                <a href="/" className="text-[#bd6097] hover:text-[#834167] text-5xl md:text-6xl lg:text-8xl xl:text-10xl ml-5">MENU</a>
+                <img
+                    src={Logo}
+                    alt="Creative Lab Logo"
+                    className="w-1/3 md:w-1/4 lg:w-1/5 mt-5 transition-all duration-300 ease-in-out"
+                />
+
+                <a href="/about-us" className="text-[#bd6097] hover:text-[#834167] text-5xl md:text-6xl lg:text-8xl xl:text-10xl mr-5">ABOUT US</a>
             </nav>
         </header>
     );
--- src/pages/ContentCreation.jsx
+++ src/pages/ContentCreation.jsx
@@ -1,6 +1,49 @@
+import foto1 from '../assets/content-creation/content-creation1.png';
+import gifImage from '../assets/content-creation/gif/gift-content-creation-webp.webp';
+
 const ContentCreation = () => {
   return (
-    <div>ContentCreation</div>
-  )
-}
-export default ContentCreation
+    <div className="flex w-full justify-center h-auto mt-10">
+      <div>
+        <div>
+          <img src={foto1} alt="Foto de ejemplo" />
+        </div>
+        <div className="ml-8">
+          <img src={gifImage} alt="Gif de ejemplo" />
+        </div>
+      </div>
+      <div>
+        <div className="ml-8">
+          <img src={gifImage} alt="Gif de ejemplo" />
+        </div>
+        <div>
+          <img src={foto1} alt="Foto de ejemplo" />
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default ContentCreation;
+
+// const ContentCreation = () => {
+//   return (
+//     <div className="flex flex-col md:flex-row w-full">
+//       <img
+//         src={foto1}
+//         alt="Foto de ejemplo"
+        // className="object-cover w-full h-auto max-h-96 md:w-1/2 md:max-h-full"
+//       />
+//       <div className="ml-8 mt-4 md:mt-0">
+//         <img
+//           src={gifImage}
+//           alt="Gif de ejemplo"
+//           className="object-cover w-full h-auto max-h-96 md:w-1/2 md:max-h-full"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ContentCreation;
