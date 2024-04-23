b2e281e7839f08f538b65cfc27e3c81fc7ee6e7a
ffran-acosta <francoaugustoacosta@gmail.com>
2024-04-23 20:18:39 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 const Header = () => {
     return (
-        <header className="bg-transparent">
-            <nav className="flex justify-between items-center">
-                <div className="text-customViolet text-xl md:text-xl lg:text-xl ml-5">
-                    Creative Lab
+        <header className="bg-customBlack">
+            <nav className="flex justify-between items-center p-6">
+                <div className="text-customViolet text-xl md:text-xl lg:text-2xl ml-5 font-extrabold">
+                    CREATIVE LAB
                 </div>
                 <div className="flex items-center">
-                    <a href="/projects" className="text-customViolet text-xl md:text-xl lg:text-xl mr-5">
-                        Projects
+                    <a href="/projects" className="text-customViolet text-xl md:text-xl lg:text-xl mr-5 font-semibold">
+                        PROJECTS
                     </a>
-                    <a href="/contact" className="text-customViolet text-xl md:text-xl lg:text-xl mr-5">
-                        Contact
+                    <a href="/contact" className="text-customViolet text-xl md:text-xl lg:text-xl mr-5 font-semibold">
+                        CONTACT
                     </a>
                 </div>
                 <div className="text-customViolet text-xl md:text-xl lg:text-xl mr-5">
